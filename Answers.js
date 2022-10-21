fb712e93d9b924c0ed99baf347b13179d93c8254
BrFern <102929013+BrFern@users.noreply.github.com>
2022-10-21 15:57:52 -0600

    Wild Wild Life answered

--- Answers.js
+++ Answers.js
@@ -9,7 +9,7 @@
 
 // Get Even
 
-for (let i=0; i <= 200; i++) {
+/*for (let i=0; i <= 200; i++) {
     if (i % 2 == 0) 
         console.log(i);
     }
@@ -21,8 +21,20 @@ for (let i=0; i<= 100; i++) {
     else if (i % 3 == 0) 
         console.log(`Fizz`)
     else (i % 5 == 0)
-        console.log(`Buzz`)
+        console.log(`Buzz`) 
 }
-//I DID THIS ONE SO FAST YAYYYY!!! 
+*/
+//I DID THIS ONE SO FAST YAYYYY!!! AND WITHOUT NOTES!
 
+//Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+plantee[2] = 5001
+wolfy [3] ="Gotham City"
+dart.push("Hawkins")
+wolfy.shift()
+wolfy.push("Gameboy")
